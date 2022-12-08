2cba15ba309dbb381de93e721c5cca3fd1c6daed
Jesse Mulderij <jesse_mulderij@hotmail.com>
2022-12-08 11:21:05 +0100

    Day 1 and 2

---
+++ Day 1 Calorie Counting copy/main.py
@@ -0,0 +1,53 @@
+def find_most_calories(t):
+
+    f = open(t)
+    lines = f.readlines()
+    highest_cals = 0
+    current_cals = 0
+
+    for line in lines:
+        if line == "\n":
+            if current_cals > highest_cals:
+                highest_cals = current_cals
+            current_cals = 0
+        else:
+            current_cals += int(line)
+
+    f.close()
+    return highest_cals
+
+
+def find_3_most_calories(t):
+
+    f = open(t)
+    lines = f.readlines()
+    three_highest_cals = [0, 0, 0]
+    current_cals = 0
+
+    for line in lines:
+        if line == "\n":
+            if current_cals > three_highest_cals[0]:
+                three_highest_cals[0] = current_cals
+                three_highest_cals.sort()
+
+            current_cals = 0
+        else:
+            current_cals += int(line)
+
+    f.close()
+    sum = 0
+    print(three_highest_cals)
+    for ii in three_highest_cals:
+        sum += ii
+    return sum
+
+
+def main():
+    x = find_most_calories("input.txt")
+    print(x)
+
+    x = find_3_most_calories("input.txt")
+    print(x)
+
+
+main()
