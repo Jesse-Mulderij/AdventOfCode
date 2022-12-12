0cd0bb2e55295c8a1f6a6d7a6a852e7547053e93
autoblack <Jesse-Mulderij@users.noreply.github.com>
2022-12-12 13:24:37 +0000

    fixup: Format Python code with Black

--- Day 3 Rucksack Reorganization/main.py
+++ Day 3 Rucksack Reorganization/main.py
@@ -1,9 +1,9 @@
-
-def convert_to_priority(ii : str):
+def convert_to_priority(ii: str):
     if ii.islower():
-        return ord(ii) - 96   
+        return ord(ii) - 96
     if ii.isupper():
         return ord(ii) - 96 + 32 + 26
+
 
 def calculate_priority_sum(t):
     f = open(t)
@@ -13,8 +13,8 @@ def calculate_priority_sum(t):
     for line in lines:
         line = line.strip()
         length = len(line)
-        for ii in line[:int(length/2)]:
-            for jj in line[int(length/2):]:
+        for ii in line[: int(length / 2)]:
+            for jj in line[int(length / 2) :]:
                 if ii == jj:
                     sum += convert_to_priority(ii)
                     break_bool = True
@@ -22,9 +22,10 @@ def calculate_priority_sum(t):
             if break_bool:
                 break
         break_bool = False
-            
+
     f.close()
     return sum
+
 
 def calculate__badge_priority_sum(t):
     f = open(t)
@@ -34,12 +35,12 @@ def calculate__badge_priority_sum(t):
     for line_num in range(len(lines)):
         if line_num % 3 == 0:
             for ii in lines[line_num]:
-                for jj in lines[line_num+1]:
+                for jj in lines[line_num + 1]:
                     if ii == jj:
-                        for kk in lines[line_num+2]:
+                        for kk in lines[line_num + 2]:
                             if ii == jj == kk:
                                 sum += convert_to_priority(ii)
-                                
+
                                 break_bool = True
                                 break
                     if break_bool:
@@ -47,7 +48,7 @@ def calculate__badge_priority_sum(t):
                 if break_bool:
                     break
             break_bool = False
-            
+
     f.close()
     return sum
 
@@ -58,5 +59,6 @@ def main():
 
     x = calculate__badge_priority_sum("Day 3 Rucksack Reorganization\\input.txt")
     print(x)
+
 
 main()
