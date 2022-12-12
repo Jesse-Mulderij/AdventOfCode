f72bfde5de0f793705d67ea37b93c09b3413be92
autoblack <Jesse-Mulderij@users.noreply.github.com>
2022-12-12 15:59:16 +0000

    fixup: Format Python code with Black

--- Day 6 Tuning Trouble/main.py
+++ Day 6 Tuning Trouble/main.py
@@ -1,26 +1,29 @@
-def unique(s : str) -> bool:
+def unique(s: str) -> bool:
     s = list(s)
     s.sort()
     for ii in range(len(s) - 1):
-        if s[ii] == s[ii+1]:
-                return False
+        if s[ii] == s[ii + 1]:
+            return False
     return True
 
-def chars_before_first_packet(t : str):
+
+def chars_before_first_packet(t: str):
     f = open(t)
     line = f.readlines()[0]
-    for ii in range(len(line)-3):
-        s = line[ii:ii+4]
+    for ii in range(len(line) - 3):
+        s = line[ii : ii + 4]
         if unique(s):
             return ii + 4
 
-def chars_before_first_message(t : str):
+
+def chars_before_first_message(t: str):
     f = open(t)
     line = f.readlines()[0]
-    for ii in range(len(line)-13):
-        s = line[ii:ii+14]
+    for ii in range(len(line) - 13):
+        s = line[ii : ii + 14]
         if unique(s):
             return ii + 14
+
 
 def main():
     x = chars_before_first_packet("Day 6 Tuning Trouble/testinput.txt")
