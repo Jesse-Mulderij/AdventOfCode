05ebd5a24d2b3aff0d9b311083b0ade4aaae317d
autoblack <Jesse-Mulderij@users.noreply.github.com>
2022-12-17 02:03:33 +0000

    fixup: Format Python code with Black

--- Day 8 Treetop Tree House/main.py
+++ Day 8 Treetop Tree House/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
-def parse_input(t : str) -> List[list]:
+
+def parse_input(t: str) -> List[list]:
     f = open(t)
     lines = f.readlines()
 
@@ -13,6 +14,7 @@ def parse_input(t : str) -> List[list]:
         for h in line.strip():
             forest[y].append(h)
     return forest
+
 
 def is_visible_from_right(forest: List[list], tree: List[int]) -> bool:
     is_visible_from_right = True
@@ -60,14 +62,16 @@ def is_visible(forest: List[list], tree: List[int]) -> bool:
             or is_visible_from_bottom(forest, tree)
         )
 
-def visual_range_to_right(forest : List[list], tree : List[int]) -> int:
+
+def visual_range_to_right(forest: List[list], tree: List[int]) -> int:
     visual_range = 1
-    for x in range(tree[0] + 1, len(forest[0])-1):
+    for x in range(tree[0] + 1, len(forest[0]) - 1):
         if forest[tree[0]][tree[1]] > forest[x][tree[1]]:
             visual_range += 1
         else:
             return visual_range
     return visual_range
+
 
 def visual_range_to_top(forest: List[list], tree: List[int]) -> bool:
     visual_range = 1
@@ -80,6 +84,7 @@ def visual_range_to_top(forest: List[list], tree: List[int]) -> bool:
             return visual_range
     return visual_range
 
+
 def visual_range_to_left(forest: List[list], tree: List[int]) -> bool:
     visual_range = 1
     xrange = list(range(1, tree[0]))
@@ -91,23 +96,36 @@ def visual_range_to_left(forest: List[list], tree: List[int]) -> bool:
             return visual_range
     return visual_range
 
+
 def visual_range_to_bottom(forest: List[list], tree: List[int]) -> bool:
     visual_range = 1
-    for y in range(tree[1] + 1, len(forest[0])-1):
+    for y in range(tree[1] + 1, len(forest[0]) - 1):
         if forest[tree[0]][tree[1]] > forest[tree[0]][y]:
             visual_range += 1
         else:
             return visual_range
     return visual_range
 
-def scenic_score(forest : List[list], tree : List[list]) -> int:
+
+def scenic_score(forest: List[list], tree: List[list]) -> int:
     ymax = len(forest)
     xmax = len(forest[0])
-    if tree[0] == 0 or tree[0] == xmax-1 or tree[1] == 0 or tree[1] == ymax-1:
+    if (
+        tree[0] == 0
+        or tree[0] == xmax - 1
+        or tree[1] == 0
+        or tree[1] == ymax - 1
+    ):
         return 0
     else:
-        scenic_score = visual_range_to_right(forest, tree) * visual_range_to_top(forest, tree) * visual_range_to_left(forest, tree) * visual_range_to_bottom(forest, tree)
+        scenic_score = (
+            visual_range_to_right(forest, tree)
+            * visual_range_to_top(forest, tree)
+            * visual_range_to_left(forest, tree)
+            * visual_range_to_bottom(forest, tree)
+        )
         return scenic_score
+
 
 def count_visible_trees(t: str) -> int:
     forest = parse_input(t)
@@ -123,20 +141,21 @@ def count_visible_trees(t: str) -> int:
 
     return num_of_visible_trees
 
-def calc_scenic_score(t : str) -> int:
+
+def calc_scenic_score(t: str) -> int:
     forest = parse_input(t)
     max_score = 0
     ymax = len(forest)
     xmax = len(forest[0])
     for x in range(xmax):
         for y in range(ymax):
-            tree = [x,y]
+            tree = [x, y]
             current_score = scenic_score(forest, tree)
             if max_score < current_score:
                 max_score = current_score
-                print([x,y])
+                print([x, y])
     return max_score
-    
+
 
 def main():
     test = "Day 8 Treetop Tree House/testinput.txt"
